5e742c814d8107b1fac72a60891e6aeda4a95137
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2024-05-03 16:09:12 -0600

    added single query by id

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,14 +1,25 @@
 const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
 
-const getContact = async (req, res) => {
-    // const result = await mongodb.getDb().DB().collection('contact').find();
-    const result = await mongodb.getDb().db().collection('contact').find();
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists);
-    });
+const getAllContacts = async (req, res) => {
+        const result = await mongodb.getDb().db().collection('contact').find();
+        result.toArray().then((lists) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists);
+        });
+    };
+
+const getContactById = async (req, res) => {
+    const id = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('contact').findOne({_id: id});
+    if (result) {
+        res.status(200).json(result);
+    } else {
+        res.status(404).json({ message: 'Contact not found' });
+    }
 };
 
 module.exports = {
-    getContact
-}
+    getAllContacts,
+    getContactById
+};
--- routes/contact.js
+++ routes/contact.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contact');
 
-router.get('/', contactController.getContact);
+router.get('/', contactController.getAllContacts);
+
+router.get('/:id', contactController.getContactById);
 
 module.exports = router
