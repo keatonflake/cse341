24b023e8b35bf100bb0afb3c02a346300c8206e8
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2024-05-19 00:17:09 -0600

    updated render link so https schemes are accepted

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API for my project delivers contacts stored in MongoDB',
   },
   host: 'cse341-15dr.onrender.com',
-  schemes: ['http'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
